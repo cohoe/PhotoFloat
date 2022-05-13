c6a664876a3db73d43c04dd8334a38e25d5625d0
Grant Cohoe <grant@grantcohoe.com>
2022-05-12 22:24:04 -0400

    fix my shit

--- scanner/PhotoAlbum.py
+++ scanner/PhotoAlbum.py
@@ -145,7 +145,7 @@ class Photo(object):
 		exif = {}
 		for tag, value in info.items():
 			decoded = TAGS.get(tag, tag)
-                        if (isinstance(value, tuple) or isinstance(value, list)) and (isinstance(decoded, str) or isinstance(decoded, unicode)) and decoded.startswith("DateTime") and len(value) >= 1:
+		    if (isinstance(value, tuple) or isinstance(value, list)) and (isinstance(decoded, str) or isinstance(decoded, unicode)) and decoded.startswith("DateTime") and len(value) >= 1:
 				value = value[0]
 			if isinstance(value, str) or isinstance(value, unicode):
 				value = value.strip().partition("\x00")[0]
@@ -260,7 +260,10 @@ class Photo(object):
 				bottom = image.size[1] - ((image.size[1] - image.size[0]) / 2)
 			image = image.crop((left, top, right, bottom))
 			gc.collect()
-		image.thumbnail((size, size), Image.ANTIALIAS)
+		try:
+			image.thumbnail((size, size), Image.ANTIALIAS)
+		except Exception as e:
+			message("error", e)
 		try:
 			# https://github.com/python-pillow/Pillow/issues/2609
 			if image.mode in ('RGBA', 'LA'):
